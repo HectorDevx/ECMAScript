cc89451ffdb935a321187d296f6c2dc41ec7fa9a
Héctor Reyes <h.reyes@idventiva.com>
2020-04-13 21:16:31 -0500

    Clase 6, ECMAScript 7

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+//-------------------------------------------------- Includes
+    // Nos permite saber su hay un elemento dentro de este valor.
+
+let numbers = [1,2,3,4,5,8,9]
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el número 7')
+} else {
+    console.log('No se encuentra el número 7 :( ')
+}
+
+
+//-------------------------------------------------- Potencia
+
+let base = 4
+let exponent = 3
+
+let result = base ** exponent
+
+console.log(result);
